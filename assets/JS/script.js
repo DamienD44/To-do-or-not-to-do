9b4b012f7a21b814a60c635984b583a228072f6d
DamienD44 <damas44@gmail.com>
2024-10-12 22:14:02 +0200

    catégorie 1 s'affiche bien, ne fonctionne pas pour la cat 2 et cat 3

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,76 +1,41 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
-
-
-
-
-
-
-
-
-
-//const array1 = [];
-
-//for (const element of array1) {
-  //console.log(element);
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-c//onst tasks = ["tache 1", "tache 2"];
-//document.querySelector("ul");
-//fetch(tasks)
-//.then ((response) => {
-  //  return response.tasks})
-//.then ((cat1) => {
-  //  for(const tache1 of tasks) {
-    //    const liElement =
-      //  document.createElement("li");
-        //liElement.innertext = tasks.tache1;
-        //ulElement.appenChild(liElement);
-    //}
-//}   
-
-
-
-
- 
-
-
-//const txt1 = document.getElementById('tache1');
-//const btn1 = document.getElementById('btn1');
-//const out1 = document.getElementById('output1');
-
-//function fun1() {
-  // out1.innerHTML= txt1.value;
-//}
-//btn1.addEventListener('click',fun1);
-
-//const txt2 = document.getElementById('tache2');
-//const btn2 = document.getElementById('btn2');
-//const out2 = document.getElementById('output2');
-
-//function fun2() {
-  // out2.innerHTML= txt2.value;
+function addTask(){
+    if(inputBox.value ===''){
+        alert("Vous devez écrire quelquechose!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
 }
-//btn2.addEventListener('click',fun2);
 
-//const txt3 = document.getElementById('tache3');
-//const btn3 = document.getElementById('btn3');
-//const out3 = document.getElementById('output3');
+const inputBox1 = document.getElementById("input-box1");
+const listContainer1 = document.getElementById("list-container1");
 
-//function fun3() {
-  // out3.innerHTML= txt3.value;
-//}
-//btn3.addEventListener('click',fun3);//
+function addTask1(){
+    if(inputBox1.value ===''){
+        alert("Vous devez écrire quelquechose!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox1.value;
+        listContainer.appendChild(li);
+    }
+}
+
+const inputBox2 = document.getElementById("input-box2");
+const listContainer2 = document.getElementById("list-container2");
+
+function addTask2(){
+    if(inputBox2.value ===''){
+        alert("Vous devez écrire quelquechose!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox2.value;
+        listContainer.appendChild(li);
+    }
+}
