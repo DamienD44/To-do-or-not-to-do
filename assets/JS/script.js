7925538f19d0b5a58b48fb0ce9531329b7e9731d
DamienD44 <damas44@gmail.com>
2024-10-15 16:17:30 +0200

    resolution du preobleme de catégorie affichais tout dans la cat 1

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,8 @@
+/* Mise en place des listes de tâche de la catégorie 1*/
+
+
+
+
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
@@ -6,11 +11,15 @@ function addTask(){
         alert("Vous devez écrire quelquechose!");
     }
     else{
-        let li = document.createElement("li");
+        const li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
     }
 }
+
+
+
+/* Mise en place des listes de tâche de la catégorie 2*/
 
 const inputBox1 = document.getElementById("input-box1");
 const listContainer1 = document.getElementById("list-container1");
@@ -20,11 +29,13 @@ function addTask1(){
         alert("Vous devez écrire quelquechose!");
     }
     else{
-        let li = document.createElement("li");
+        const li = document.createElement("li");
         li.innerHTML = inputBox1.value;
-        listContainer.appendChild(li);
+        listContainer1.appendChild(li);
     }
 }
+
+/* Mis en place de la listes des tâche de la catégorie 3*/
 
 const inputBox2 = document.getElementById("input-box2");
 const listContainer2 = document.getElementById("list-container2");
@@ -34,8 +45,8 @@ function addTask2(){
         alert("Vous devez écrire quelquechose!");
     }
     else{
-        let li = document.createElement("li");
+        const li = document.createElement("li");
         li.innerHTML = inputBox2.value;
-        listContainer.appendChild(li);
+        listContainer2.appendChild(li);
     }
 }
